72054a0b239e9bb35a040a8bce81fcf6995e4793
Maksym Pakula <gargantadoto@gmail.com>
2026-02-03 03:22:42 +0200

    Add main page

---
+++ kitchen/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from kitchen.views import (
+    index
+)
+
+
+urlpatterns = [
+    path("", index, name="main-page"),
+]
+
+app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import DishType, Cook, Ingredient, Dish
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_dish_types = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+    }
+
+    return render(request, "kitchen/index.html", context)

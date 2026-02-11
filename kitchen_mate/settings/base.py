7536f3a88dc5504bb3bb376521c0b3470688f301
Maksym Pakula <gargantadoto@gmail.com>
2026-02-11 17:39:50 +0200

    refactor settings structure for production and add environment loading

--- kitchen_mate/settings/base.py
+++ kitchen_mate/settings/base.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -11,10 +11,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-m699kltz_94-z2dd5sg9gi@9w@)x!inog^cs@^t8_mc4hhst$g")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-
-ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -24,7 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "kitchen",
+    "kitchen.apps.KitchenConfig",
     "crispy_forms",
     "crispy_bootstrap5",
 ]
@@ -60,15 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "kitchen_mate.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
